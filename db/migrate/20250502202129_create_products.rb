677dd280cd97c6194d1cbcea4178f0b44e1311b2
fillson22 <fillson22051983@gmail.com>
2025-05-02 20:25:10 +0000

    create model User, Product

---
+++ db/migrate/20250502202129_create_products.rb
@@ -0,0 +1,10 @@
+class CreateProducts < ActiveRecord::Migration[8.0]
+  def change
+    create_table :products do |t|
+      t.string :name
+      t.string :text
+
+      t.timestamps
+    end
+  end
+end
---
+++ db/schema.rb
@@ -0,0 +1,18 @@
+ActiveRecord::Schema[8.0].define(version: 2025_05_02_202129) do
+
+  enable_extension "pg_catalog.plpgsql"
+
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.string "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "username"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+end
