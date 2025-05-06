c930af775b1955107dd9e8eba0ceaa2d7ad5ba56
fillson22 <fillson22051983@gmail.com>
2025-05-06 19:37:32 +0000

    add validates for models and association

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,7 @@
 class Product < ApplicationRecord
+
+    belongs_to :user
+
+    validates :name, :text, :user, presence: true
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+
+    has_many :products, dependent: :destroy
+
+    validates :email, :username, presence: true
+    validates :email, :username, uniqueness: true
+
+
 end
